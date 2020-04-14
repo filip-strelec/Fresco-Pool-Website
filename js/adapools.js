0a6a18c26dbf4789ed1db9c11374a73cfcc843c2
filip4428 <filip.strelec@speck.agency>
2020-04-14 14:15:44 +0200

    Minor fix

--- js/adapools.js
+++ js/adapools.js
@@ -1,18 +1,12 @@
-$.getJSON('https://adapools.org/cache/pools/bd1d1aafead6f652f76f5921b4ffdb429d7eb9d5322d0f4700f4f70f997c5a82.json', function(data) {
-
-
-
+$.getJSON('https://adapools.org/cache/pools/bd1d1aafead6f652f76f5921b4ffdb429d7eb9d5322d0f4700f4f70f997c5a82.json', (data)=> {
 
     // $.each( data, function( i, val ) { $( '#' + data.id + '_' + i ).text(val); });
-
 
 console.log(data)
 
 $("#pool-adress").text(data.id)
 $("#pool-tag").text(data.tag)
 $("#pool-total-stake").text(data.total_stake)
-
-
 
     });
 
