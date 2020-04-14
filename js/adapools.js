0a4319ed0281ef2dd07f0a7db25c2385b32023c1
filip4428 <filip.strelec@speck.agency>
2020-04-14 14:14:35 +0200

    In dev

--- js/adapools.js
+++ js/adapools.js
@@ -1,3 +1,29 @@
 $.getJSON('https://adapools.org/cache/pools/bd1d1aafead6f652f76f5921b4ffdb429d7eb9d5322d0f4700f4f70f997c5a82.json', function(data) {
-    $.each( data, function( i, val ) { $( '#' + data.id + '_' + i ).text(val); });
+
+
+
+
+    // $.each( data, function( i, val ) { $( '#' + data.id + '_' + i ).text(val); });
+
+
+console.log(data)
+
+$("#pool-adress").text(data.id)
+$("#pool-tag").text(data.tag)
+$("#pool-total-stake").text(data.total_stake)
+
+
+
+    });
+
+    //script for scroll
+
+    $(window).scroll(function(){
+        var scrollPos = $(document).scrollTop();
+        console.log(scrollPos);
+
+        $(".cardano-logo").css("transform", `rotate(${scrollPos}deg)`)
+
+
+
     });
