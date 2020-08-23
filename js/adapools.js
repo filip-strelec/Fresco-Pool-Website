c6eb7af804e4d81a0b11a37775b6037bd505d20b
filip4428 <filip.strelec@speck.agency>
2020-08-24 01:25:45 +0200

    c

--- js/adapools.js
+++ js/adapools.js
@@ -63,7 +63,9 @@ async function getLiveStats() {
 
 
     // $liveStake = ($liveStake / 1000000 / 1000000).toFixed(2); //in million ADA
-    $(".blocks-produced").html(blocksEpoch+blocksLifetime);
+    // $(".blocks-produced").html(blocksEpoch+blocksLifetime);
+    $(".blocks-produced").html("9");
+
     $(".total-stake").html((totalStake/ 1000000 / 1000000).toFixed(2)+" M");
     $(".active-stake").html((activeStake/ 1000000 / 1000000).toFixed(2)+" M");
     $(".blocks-estimated").html((blocksEstimated))
